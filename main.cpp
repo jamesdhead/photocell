cebaee45d37e629662e0d799b82cbcc1439ff4e4
james <james@headsrv.jamesdhead.com>
2017-03-01 04:38:51 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <SD.h>  //SD Card libraries
+#include <SPI.h  //SPI library
+#include <iostream> //allows namespace and others
+#include <PubSubCLient.h>
+using namespace std;
+
+int photoRPin = 0;
+int = minLight;  //calibrates
+int = maxLight;  //calibrates
+int = lightLevel;
+int = adjustedLightlevel;
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
